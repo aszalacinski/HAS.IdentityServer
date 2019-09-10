622d14b5aa0659dcac7cb22e1be3ac84ff995e3b
Aarron Szalacinski <aarron.szalacinski@happyappsoftware.com>
2019-09-09 21:35:02 -0400

    Implementing ASP.NET Identity

--- Startup.cs
+++ Startup.cs
@@ -1,18 +1,20 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
-
-using System;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.MongoDb;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using IdentityUser = Microsoft.AspNetCore.Identity.MongoDb.IdentityUser;
 
 namespace HAS.IdentityServer
 {
     public class Startup
     {
         public IHostingEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
 
         public Startup(IHostingEnvironment environment)
         {
@@ -21,14 +23,27 @@ namespace HAS.IdentityServer
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CloudSettings>(Configuration.GetSection("CloudSettings"));
+            services.AddSingleton<IUserStore<IdentityUser>>(provider =>
+            {
+                var options = provider.GetService<IOptions<CloudSettings>>();
+                var client = new MongoClient(options.Value.DBConnectionString_MongoDB);
+                var database = client.GetDatabase(options.Value.DBConnectionString_MongoDB_DatabaseName);
+
+                return UserStore<IdentityUser>.CreateAsync(database).GetAwaiter().GetResult();
+            });
+
+            services.AddIdentity<IdentityUser>();
+
             // uncomment, if you want to add an MVC-based UI
             services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
-            
+
             var builder = services.AddIdentityServer()
                 .AddInMemoryIdentityResources(Config.GetIdentityResources())
                 .AddInMemoryApiResources(Config.GetApis())
                 .AddInMemoryClients(Config.GetClients())
-                .AddTestUsers(Config.GetUsers());
+                //.AddTestUsers(Config.GetUsers());
+                .AddAspNetIdentity<IdentityUser>();
 
             if (Environment.IsDevelopment())
             {
@@ -46,11 +61,12 @@ namespace HAS.IdentityServer
             {
                 app.UseDeveloperExceptionPage();
             }
-            
+
+            app.UseIdentityServer();
+
             // uncomment if you want to support static files
             app.UseStaticFiles();
 
-            app.UseIdentityServer();
 
             // uncomment, if you want to add an MVC-based UI
             app.UseMvcWithDefaultRoute();
